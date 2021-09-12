a30ba9d04afd3243b832b33ed09197a34f8b55d0
ResminiLoris <resminiloris@gmail.com>
2021-09-12 12:51:12 +0200

    milestone_3_1/2

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,11 +5,17 @@
       <ul>
           <li v-for="(movie,index) in movies" :key="index" >
               <div>
+                  <!-- poster -->
+                  <div><img class="poster" :src="`${posterUri}${posterWidth}${movie.poster_path}`" alt=""></div>
+                  <!-- titolo -->
                   <div><strong>titolo:</strong>{{movie.title}}</div>
+                  <!-- titolo originale -->
                   <div><strong>titolo originale:</strong>:{{movie.original_title}}</div>
-                  <div v-if="movie.original_language=== 'en'"><strong>lingua originale:</strong><img src="../assets/img/gb.svg" alt="eng"></div>
-                  <div v-else-if="movie.original_language=== 'it'"><strong>lingua originale:</strong><img src="../assets/img/it.svg" alt="ita"></div>
+                  <!-- lingua -->
+                  <div v-if="movie.original_language=== 'en'"><strong>lingua originale:</strong><img class="leng-icon" src="../assets/img/gb.svg" alt="eng"></div>
+                  <div v-else-if="movie.original_language=== 'it'"><strong>lingua originale:</strong><img class="leng-icon" src="../assets/img/it.svg" alt="ita"></div>
                   <div v-else ><strong>lingua originale:</strong>{{movie.original_language}}</div>
+                  <!-- voto -->
                   <div><strong>voto:</strong>{{movie.vote_average}}</div>
                 </div>
           </li>
@@ -19,11 +25,17 @@
       <ul>
           <li v-for="(serie,index) in series" :key="index" >
               <div>
+                  <!-- poster -->
+                  <div><img class="poster" :src="`${posterUri}${posterWidth}${serie.poster_path}`" alt=""></div>
+                  <!-- titolo -->
                   <div><strong>titolo:</strong>{{serie.name}}</div>
+                  <!-- titolo originale -->
                   <div><strong>titolo originale:</strong>:{{serie.original_name}}</div>
-                  <div v-if="serie.original_language=== 'en'"><strong>lingua originale:</strong><img src="../assets/img/gb.svg" alt="eng"></div>
-                  <div v-else-if="serie.original_language=== 'it'"><strong>lingua originale:</strong><img src="../assets/img/it.svg" alt="ita"></div>
+                  <!-- lingua -->
+                  <div v-if="serie.original_language=== 'en'"><strong>lingua originale:</strong><img class="leng-icon" src="../assets/img/gb.svg" alt="eng"></div>
+                  <div v-else-if="serie.original_language=== 'it'"><strong>lingua originale:</strong><img class="leng-icon" src="../assets/img/it.svg" alt="ita"></div>
                   <div v-else ><strong>lingua originale:</strong>{{serie.original_language}}</div>
+                  <!-- voto -->
                   <div><strong>voto:</strong>{{serie.vote_average}}</div>
                 </div>
           </li>
@@ -35,6 +47,12 @@
 export default {
     name: "Main",
     props:['movies', 'series', 'searchTerm'],
+    data(){
+        return{
+            posterUri: 'https://image.tmdb.org/t/p',
+            posterWidth: '/w342',
+        }
+    },
 }
 </script>
 
@@ -43,13 +61,11 @@ ul{
     list-style-type: none;
     li{
         margin-bottom: 20px;
-        border: 1px solid black;
         padding: 10px;
-        
-        img{
-            width: 20px;
-        }
     }
 }
 
+.leng-icon{
+    width: 20px;
+}
 </style>
